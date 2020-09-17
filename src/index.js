9f2adaf655d9f267b86840bb3ecbd5edde411bb4
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-17 14:44:38 -0600

    Adds HelloK component;

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloK from './components/HelloK';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <HelloK />
   </React.StrictMode>,
   document.getElementById('root')
 );
